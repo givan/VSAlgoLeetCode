aa542fe4de1f59c938ef9300e7b60f0879788c53
George Ivanov <george.ivanov@gmail.com>
2020-01-21 12:07:18 -0800

    Alternating bits
    Positive integer has alternating bits

---
+++ Algo.Tests/BinaryNumberAlternatingBitsTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algo.Tests
+{
+    [TestClass]
+    public class BinaryNumberAlternatingBitsTests
+    {
+        [TestMethod]
+        public void PositiveOneBit()
+        {
+            // arrange
+            // 1 is 1 binary and hence is a solution
+            int input = 1;
+            bool expectedResult = true;
+
+            // act
+            BinaryNumberAlternatingBits alternatingBinaryNumbers = new BinaryNumberAlternatingBits();
+            bool result = alternatingBinaryNumbers.HasAlternatingBits(input);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        public void NegativeOneBit()
+        {
+            // arrange
+            // 0 is 0 binary (not a solution)
+            int input = 0;
+            bool expectedResult = false;
+
+            // act
+            BinaryNumberAlternatingBits alternatingBits = new BinaryNumberAlternatingBits();
+            bool result = alternatingBits.HasAlternatingBits(input);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void PositiveTwoBitsNumber()
+        {
+            // arrange
+            // 2 is 10 binary and hence is a solution
+            int input = 2;
+            bool expectedResult = true;
+
+            // act
+            BinaryNumberAlternatingBits alternatingBinaryNumbers = new BinaryNumberAlternatingBits();
+            bool result = alternatingBinaryNumbers.HasAlternatingBits(input);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void NegativeTwoBitsNumber()
+        {
+            // arrange
+            // 3 is 11 binary and hence is NOT a solution
+            int input = 3;
+            bool expectedResult = false;
+
+            // act
+            BinaryNumberAlternatingBits alternatingBinaryNumbers = new BinaryNumberAlternatingBits();
+            bool result = alternatingBinaryNumbers.HasAlternatingBits(input);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void PositiveThreeBitsNumber()
+        {
+            // arrange
+            // 5 is 101 binary and hence is a solution
+            int input = 5;
+            bool expectedResult = true;
+
+            // act
+            BinaryNumberAlternatingBits alternatingBinaryNumbers = new BinaryNumberAlternatingBits();
+            bool result = alternatingBinaryNumbers.HasAlternatingBits(input);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void NegativeThreeBitsNumber()
+        {
+            // arrange
+            // 7 is 111 binary and hence is NOT a solution
+            int input = 7;
+            bool expectedResult = false;
+
+            // act
+            BinaryNumberAlternatingBits alternatingBinaryNumbers = new BinaryNumberAlternatingBits();
+            bool result = alternatingBinaryNumbers.HasAlternatingBits(input);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Algo/BinaryNumberAlternatingBits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algo
+{
+    // Solution for the problem: https://leetcode.com/problems/binary-number-with-alternating-bits/
+    // Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+    public class BinaryNumberAlternatingBits
+    {
+        public bool HasAlternatingBits(int n)
+        {
+            if (n <= 0) return false;
+
+            // conver the int into a string with the bits value at each char
+            string numberBits = Convert.ToString(n, 2); // convert the int to an array using 2-based system
+
+            // we start with the assumption that the bits are alternating until we find two consequtive bits which are equal
+            bool areAlternating = true;
+            for (int i = 1; i < numberBits.Length; i++)
+            {
+                if (numberBits[i - 1] == numberBits[i])
+                {
+                    areAlternating = false;
+                    break;
+                }
+            }
+
+            return areAlternating;
+        }
+    }
+}
