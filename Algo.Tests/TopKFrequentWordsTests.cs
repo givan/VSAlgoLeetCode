7619d63f9111f2f1be77b7ee0e7b91b6801ae57a
George Ivanov <george.ivanov@gmail.com>
2020-01-21 11:21:05 -0800

    Top K most frequent words
    Top K most frequent words problem solved with SortedSet

---
+++ Algo.Tests/TopKFrequentWordsTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algo.Tests
+{
+    // tests for the problem: https://leetcode.com/problems/top-k-frequent-words/
+    [TestClass]
+    public class TopKFrequentWordsTests
+    {
+        [TestMethod]
+        public void Top2MostFrequentWords()
+        {
+            // arrange
+            // Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+            string[] words = new [] { "i", "love", "leetcode", "i", "love", "coding" };
+            int k = 2;
+            List<string> expected = new List<string>() { "i", "love" };
+
+            // act
+            List<string> mostFreqWords = new List<string>(TopKFrequentWords.TopKFrequent(words, k));
+
+            // assert
+            CollectionAssert.AreEqual(expected, mostFreqWords);
+        }
+
+        [TestMethod]
+        public void Top4MostFrequentWords()
+        {
+            // arrange
+            string[] words = new[] {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" };
+            int k = 4;
+
+            List<string> expectedWords = new List<string>() { "the", "is", "sunny", "day" };
+
+            // act
+            IList<string> result = TopKFrequentWords.TopKFrequent(words, k);
+            List<string> mostFequentWords = new List<string>(result);
+
+            // assert
+            CollectionAssert.AreEqual(expectedWords, mostFequentWords);
+        }
+    }
+}
---
+++ Algo/TopKFrequentWords.cs
@@ -0,0 +1,82 @@
+// Solution for https://leetcode.com/problems/top-k-frequent-words/
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+public static class TopKFrequentWords
+{
+    public static IList<string> TopKFrequent(string[] words, int k)
+    {
+        List<string> result = new List<string>();
+
+        if (words == null || words.Length == 0) return result;
+        if (k <= 0) return result;
+
+        // first iterate over all words and count how many times each word counts
+        Dictionary<string, int> wordsToCount = new Dictionary<string, int>();
+        foreach (var word in words)
+        {
+            wordsToCount.TryGetValue(word, out int count);
+            count++;
+            wordsToCount[word] = count;
+        }
+
+        // then in a sorted set, keep the top k used words
+        SortedSet<WordCount> topKFoundWords = new SortedSet<WordCount>(new WordCountComparer());
+        foreach (var word in wordsToCount.Keys)
+        {
+            WordCount currentWordCount = new WordCount(word, wordsToCount[word]);
+            topKFoundWords.Add(currentWordCount);
+
+            if (topKFoundWords.Count > k)
+            {
+                // remove the min element
+                topKFoundWords.Remove(topKFoundWords.Min);
+            }
+        }
+
+        // finally iterate over the sorted set to retrieve the top k used words
+        while (topKFoundWords.Any())
+        {
+            WordCount currentMax = topKFoundWords.Max;
+            result.Add(currentMax.Word);
+            topKFoundWords.Remove(currentMax);
+        }
+
+        return result;
+    }
+}
+
+class WordCount
+{
+    public string Word { get; private set; }
+    public int Count { get; private set; }
+
+    public WordCount(string word, int count)
+    {
+        Word = word;
+        Count = count;
+    }
+}
+
+class WordCountComparer : Comparer<WordCount>
+{
+    public override int Compare([AllowNull] WordCount left, [AllowNull] WordCount right)
+    {
+        if (left == null && right == null) return 0;
+        if (left == null && right != null) return 1;
+        if (left != null && right == null) return -1;
+
+        if (left.Count == right.Count)
+        {
+            // If two words have the same frequency, then the word with the lower alphabetical order comes first.
+            // becase we're generating decreasing sequence, we want to first return the bigger and then the smaller string
+            // based on their alphabetical order; for example, if we have "i" and "love", then "i" has lower alphabetical order than "love"
+            // however the requirement here is to return "i" before "love" hence for the wordCount element we need to inverse by sign
+            // the comparison result from the word values
+            return left.Word.CompareTo(right.Word) * (-1);
+        }
+
+        return left.Count.CompareTo(right.Count);
+    }
+}
